025b342a52fd136cd219bd0c7557c8617cd6a083
Ryan Doyle <doylecodes@gmail.com>
2019-11-14 16:40:44 -0800

    working on gallery component

--- web/src/components/Gallery.js
+++ web/src/components/Gallery.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import { css } from '@emotion/core';
 
 const galleryContainer = css`
@@ -9,13 +10,56 @@ const galleryContainer = css`
   box-sizing: border-box;
 `;
 
-const Gallery = () => {
-  const [showLightbox, toggleLightbox] = useState(true);
+const lightbox = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(0, 0, 0, 0.9);
+`;
+
+const Gallery = (props) => {
+  const [showLightbox, toggleLightbox] = useState(false);
+  
+  const allImageQuery = useStaticQuery(
+    graphql`
+      query AllImageQuery {
+        allSanityImageAsset {
+          edges {
+            node {
+              square: fixed(height: 300, width: 300) {
+                src
+                srcSet
+                srcSetWebp
+                width
+                srcWebp
+                height
+              }
+              normal: fixed {
+                src
+                srcSet
+                srcSetWebp
+                width
+                srcWebp
+                height
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  const imagesArray = allImageQuery.allSanityImageAsset.edges;
+  console.log(imagesArray);
   return (
     <div css={galleryContainer} onClick={() => { toggleLightbox(!showLightbox) }}>
-      <p>
-        {showLightbox ? 'true' : 'false'}
-      </p>
+      {imagesArray.map(image => (
+        <p>{image.node.normal.src}</p>
+      ))}
+      { showLightbox && (
+        <div css={lightbox}>I'm in the lightbox</div>
+      )}
     </div>
   );
 };
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Gallery from '../components/Gallery'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Gallery />
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Gallery />
+    </Layout>
+  )
+}
 
 export default IndexPage
